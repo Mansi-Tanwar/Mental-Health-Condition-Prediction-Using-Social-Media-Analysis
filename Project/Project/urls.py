21b7d3d8e8ad2381c64f16e49230fc1b32d747e1
Mansi-Tanwar <148472134+Mansi-Tanwar@users.noreply.github.com>
2024-11-17 20:32:08 +0530

    "Updated urls.py"

--- Project/Project/urls.py
+++ Project/Project/urls.py
@@ -1,7 +1,7 @@
 # Project/urls.py
 from django.contrib import admin
 from django.urls import path, include
-from survey import views
+from Survey import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
