2878c546cdd0fda105cfcae08c8c567873f69321
Mansi-Tanwar <148472134+Mansi-Tanwar@users.noreply.github.com>
2024-11-17 20:51:00 +0530

    "Updated file"

--- Project/Survey/migrations/0002_alter_surveyresponse_age.py
+++ Project/Survey/migrations/0002_alter_surveyresponse_age.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('survey', '0001_initial'),
+        ('Survey', '0001_initial'),
     ]
 
     operations = [
