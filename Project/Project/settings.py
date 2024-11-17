d9f3ec3a9d365e6c5af472c0527fbde98618adf0
Mansi-Tanwar <148472134+Mansi-Tanwar@users.noreply.github.com>
2024-11-18 00:30:05 +0530

    Update settings.py

--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'Survey',
+    'common.djangoapps.Survey',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
