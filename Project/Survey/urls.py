a02ebdd8667bee388f593a9ccaf229486a24acd7
Mansi-Tanwar <148472134+Mansi-Tanwar@users.noreply.github.com>
2024-11-17 20:29:49 +0530

    "Updated file"

--- Project/Survey/urls.py
+++ Project/Survey/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('survey/', views.survey_show, name='survey_show'),
-    path('survey/output/', views.survey_output, name='survey_output'),  # URL for the output page
+    path('Survey/', views.survey_show, name='survey_show'),
+    path('Survey/output/', views.survey_output, name='survey_output'),  # URL for the output page
 ]
