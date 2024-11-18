5d9d869db0d5e091f8f82033847cbce79e16c088
Mansi-Tanwar <148472134+Mansi-Tanwar@users.noreply.github.com>
2024-11-18 05:49:09 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,5 +7,9 @@ python -m pip install --no-cache-dir -r requirements.txt
 
 cd Project
 
+# Print the directory structure to confirm Survey's location
+echo "Directory structure:"
+ls -R
+
 python manage.py collectstatic --no-input
 python manage.py migrate
