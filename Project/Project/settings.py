5af3364af6c6bbe55a145148bc651f4a91067702
Mansi-Tanwar <148472134+Mansi-Tanwar@users.noreply.github.com>
2024-11-25 14:19:05 +0530

    Update settings.py

--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -35,7 +35,7 @@ if not os.path.isdir(os.path.join(BASE_DIR, 'Survey')):
 # Application definition
 
 INSTALLED_APPS = [
-    'Survey',
+    'Project.Survey',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
